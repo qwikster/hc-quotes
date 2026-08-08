75bb4711c847d92f66e4c4ca95d799c0c72daebd
parking_turkeys <roya1060wien@gmail.com>
2026-08-08 17:37:10 -0400

    i hate javascript

--- static/js/script.js
+++ static/js/script.js
@@ -25,6 +25,6 @@ function add_quote_to_page(id, quote, author, vote_count)
 	const quote_html = `<DIV CLASS="quote-contain"><BLOCKQUOTE><P>${quote}</P></BLOCKQUOTE><P>-- ${author}</P></DIV><DIV CLASS="vote_buttons"><BUTTON onclick='vote(true, "${id}")'>Upvote</BUTTON><SPAN id="vote_count">${vote_count}</SPAN><BUTTON onclick='vote(false, "${id}")'>Downvote</BUTTON></DIV>`;
 	var div = document.createElement("div");
 	div.innerHTML = quote_html.trim();
-	document.GetElementsByTagName("body")[0].appendChild(div.firstChild); //copied from somewhere i think
+	document.GetElementById("quotes-all").appendChild(div.firstChild); //copied from somewhere i think
 
 }
