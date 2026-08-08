5e55d0041c34b763af62a82755c3fafb2368f713
qwikster <ocoincs@gmail.com>
2026-08-08 17:20:18 -0400

    balalalal i hate jaavascapasrasnrap

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,26 @@
 const VOTE_URL = "/vote";
 const RAND_URL = "/quotes";
 
-function vote(is_up, quote_id)
+async function vote(is_up, quote_id)
 {
 	//make URL
-	
+
 	const url = `${VOTE_URL}?id=${quote_id}&up=${is_up}`;
 	const response = await fetch(url, { method: "POST" });
 	return 1;
 };
 
-function add_random_quotes(count)
+async function add_random_quotes(count)
 {
 	const response = await fetch(RAND_URL);
-	const response_array = JSON.parse(await response.json());
+	const response_array = await response.json();
 	for (i = 0; i < response_array.length; i++) {
 		let q = response_array[i];
 		add_quote_to_page(q.id, q.quote, q.author, q.votes);
 	};
 };
-		
-	
 
-function add_quote_to_page(id, quote, author, vote_count) 
+function add_quote_to_page(id, quote, author, vote_count)
 {
 	const quote_html = `<DIV CLASS="quote-contain"><BLOCKQUOTE><P>${quote}</P></BLOCKQUOTE><P>-- ${author}</P></DIV><DIV CLASS="vote_buttons"><BUTTON onclick='vote(true, "${id}")'>Upvote</BUTTON><SPAN id="vote_count">${vote_count}</SPAN><BUTTON onclick='vote(false, "${id}")'>Downvote</BUTTON></DIV>`;
 	var div = document.createElement("div");
@@ -30,7 +28,3 @@ function add_quote_to_page(id, quote, author, vote_count)
 	document.GetElementsByTagName("body")[0].appendChild(div.firstChild); //copied from somewhere i think
 
 }
-
-	
-
-
