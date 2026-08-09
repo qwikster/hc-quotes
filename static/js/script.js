84b83e469a7274284e341a7ff074f8893cea6d78
parking_turkeys <roya1060wien@gmail.com>
2026-08-08 20:25:44 -0400

    disabling upvote/downvote button gay

--- static/js/script.js
+++ static/js/script.js
@@ -10,10 +10,11 @@ async function vote(is_up, quote_id)
 	const response_obj = await response.json();
 	document.getElementById("vote_count_" + quote_id).innerHTML = response_obj.votes;
 
-	voteButtons = document.querySelector("div.vote_buttons button");
+	voteButtons = document.querySelector("div.vote-buttons button");
 	for (let i = 0; i < voteButtons.length; i++) {
 		voteButtons[i].disabled = true;
 	};
+	
 
 
 	return 1;
@@ -25,12 +26,17 @@ async function add_random_quotes(count)
 	const response_array = await response.json();
 	for (i = 0; i < response_array.length; i++) {
 		let q = response_array[i];
-		add_quote_to_page(q.id, q.quote, q.author, q.votes);
+		add_quote_to_page(q.id, q.quote, q.author, q.votes, q.voted);
 	};
 };
 
-function add_quote_to_page(id, quote, author, vote_count)
+function add_quote_to_page(id, quote, author, vote_count, is_voted)
 {
+	if (is_voted) {
+		let disabled = "disabled";
+	} else {
+		let disabled = "";
+	};
   const quote_html = `
   <div class="vote-cont">
     <a class="hidelink" href="/q/${id}" target="_self">
@@ -40,13 +46,13 @@ function add_quote_to_page(id, quote, author, vote_count)
         </div>
         </a>
         <div class="vote-buttons">
-            <button class="vote-btn upvote" aria-label="up" onclick="vote(true, '${id}')">
+            <button ${disabled} class="vote-btn upvote q${id}" aria-label="up" onclick="vote(true, '${id}')">
                 <svg viewBox="0 0 24 24">
                     <path d="M4 14h6v8h4v-8h6L12 4 4 14z"/>
                 </svg>
             </button>
             <span class="vote-count" id="vote_count_${id}">${vote_count}</span>
-            <button class="vote-btn downvote" aria-label="down" onclick="vote(false, '${id}')">
+            <button ${disabled} class="vote-btn downvote q${id}" aria-label="down" onclick="vote(false, '${id}')">
                 <svg viewBox="0 0 24 24">
                     <path d="M20 10h-6V2h-4v8H4l8 10 8-10z"/>
                 </svg>
@@ -59,3 +65,5 @@ function add_quote_to_page(id, quote, author, vote_count)
 	document.getElementById("quotes-all").appendChild(div.firstChild); //copied from somewhere i think
 
 }
+
+
